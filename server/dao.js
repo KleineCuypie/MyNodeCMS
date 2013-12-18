4bfd983999cc9b6b69aa627bec7eb596723605e5
Tim Decuypere <tim-decuypere@hotmail.com>
2013-12-18 14:38:35 +0100

    Added insert functions + testing with get function and callbackfunction

--- server/dao.js
+++ server/dao.js
@@ -1,6 +1,5 @@
-
 var mysql = require('mysql');
-
+var strQuery = "";
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -16,10 +15,33 @@ module.exports.init = function(){
 	console.log('MySQL running');
 };
 
-module.exports.insertPage = function(name){
-		connection.query("INSERT INTO  garagevlieghetest.page (name) VALUES ('" + name + "');" , function(err, rows, fields){
-			console.log(err);
-		});
+module.exports.addPage = function(name){
+	strQuery = "INSERT INTO page (name) VALUES ('" + name + "');";
+	connection.query(strQuery, function(err, rows, fields){
+		if (err != null){console.log(err);};
+	});
+};
+
+module.exports.addPageAttribute = function(page, attribute, name){
+	strQuery = "INSERT INTO pageAttribute (page, attribute, name) VALUES (" + page + "," + attribute + ",'" + name + "');";
+	connection.query(strQuery, function(err, rows, fields){
+		if (err != null){console.log(err);};
+	});
+};
+
+module.exports.addPageAttributeContent = function(pageAttribute, content){
+	strQuery = "INSERT INTO pageAttributeContent (pageAttribute, content) VALUES (" + pageAttribute + ",'" + content + "');";
+	connection.query(strQuery, function(err, rows, fields){
+		if (err != null){console.log(err);};
+	});
+};
+
+module.exports.getAttributes = function(callback){
+	strQuery = "SELECT * FROM attribute;";
+	connection.query(strQuery, function(err, rows, fields){
+		if (err != null){console.log(err);};
+		callback(rows);
+	});
 };
 
 module.exports.closeConnection = function(){
--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 var express = require('express'),
+	// path = require('path'),
 	dao = require('./dao');
 	
 var server = express();
+// server.use(express.static(__dirname + '/client'));
+server.configure(function () {
+    server.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+});
+// server.set('views', __dirname + '/client');
+// server.engine('html', require('jade').renderFile);
 server.listen(3000);
-
 
 dao.init();
 
@@ -11,11 +17,29 @@ server.get('/heykes', function(request, response){
 	response.send('welkom op de pagina');
 });
 
-server.get('/add/:param', function(request, response){
-	dao.insertPage(request.params.param + "blabla");
+server.get('/add/:pageName', function(request, response){
+	dao.addPage(request.params.pageName);
 	response.send(200);
 });
 
+server.get('/addPageAttribute/:pageId/:attributeId/:name', function(request, response){
+	dao.addPageAttribute(request.params.pageId, request.params.attributeId, request.params.name);
+	response.send(200);
+});
+
+server.get('/addPageAttributeContent/:pageAttributeId/:content', function(request, response){
+	dao.addPageAttributeContent(request.params.pageAttributeId, request.params.content);
+	response.send(200);
+	// response.render('index');
+});
+
+server.get('/getAttributes', function(request, response){
+	dao.getAttributes(function(rows){
+		response.send(rows);
+	});
+	// response.send("please wait");
+	// response.render('index');
+});
 
 //dao.closeConnection();
 
